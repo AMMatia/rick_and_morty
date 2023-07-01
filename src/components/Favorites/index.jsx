46dba30bd4b71fe3700b7e3b1b83e76c9ace493a
AMMatia <adrianm.m-@hotmail.com>
2023-07-01 12:57:29 -0300

    Correciones 2.0

--- src/components/Favorites/index.jsx
+++ src/components/Favorites/index.jsx
@@ -1,13 +1,16 @@
 import Card from '../Card/Card';
 import styles from '../Card/Card.module.css'
 import { connect } from 'react-redux';
+import { removeFav } from '../../redux/actions';
 
 export  function Favorites(props){
 
     if (!props.myFavorites || props.myFavorites.length === 0) {
         return <p>No hay favoritos</p>;
      }
-    
+     const handleClose = (id) => {
+        props.removeFav(id);
+      };
     return(
         <div style={{position:'relative',flexDirection: 'column'}}>
             <div className={styles.cardContainer}>
@@ -19,22 +22,26 @@ export  function Favorites(props){
                 status={char.status}
                 species={char.species}
                 gender={char.gender}
-                origin={char.origin.name}
                 image={char.image}
-                onClose={props.onClose}
+                onClose={handleClose}
+                myFavorites={props.myFavorites}
                 />
                 ))}
             </div>
         </div>
     )
 }
-
+export function mapDispatchToProps (dispatch){
+    return {
+       removeFav:(id)=>dispatch(removeFav(id)),
+    }
+ }
 export function mapStateToProps(state){
     return{
         myFavorites: state.myFavorites,
     }
  }
  
- export default connect(mapStateToProps, null)(Favorites);
+ export default connect(mapStateToProps, mapDispatchToProps)(Favorites);
  
 
